185b13dc2540c976d976951be49321cc09efaee7
Kunal <148614899+Kunal0234@users.noreply.github.com>
2025-05-26 21:42:31 +0530

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  devIndicators: false,
+  images: {
+    domains: ["ap-south-1.graphassets.com"],
+  },
+};
+
+export default nextConfig;
